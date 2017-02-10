ce23299eee7c5b13353d2a9e67d18e196d690730
Luis Carlin <luiscarlin@gmail.com>
2017-02-10 17:31:32 -0500

    aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,2 +1,6 @@
-# Launch chrome
-alias chrome='google-chrome'
+alias chrome='google-chrome'  # start chrome
+
+alias ll='ls -lah'            # long list, show all, human readable units
+alias ldot='ls -ld .*'        # long list, show only ones starting with dot
+
+alias cd..='cd ..'            # forgive missing whitespace
