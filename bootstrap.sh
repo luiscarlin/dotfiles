1f14f6eea5e822e65c52cb6b9dafabffa8fd4c82
Luis Carlin <luiscarlin@gmail.com>
2017-02-09 23:10:44 -0500

    create simlinks

---
+++ bootstrap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+current_dir=$(dirname $(readlink -f $0))
+
+ln -sfn ${current_dir}/.zsh ~/.zsh
+
+
+ln -sf ${current_dir}/.bashrc ~/.bashrc
+ln -sf ${current_dir}/.gitconfig ~/.gitconfig
+ln -sf ${current_dir}/.zshrc ~/.zshrc
+
+echo done
