e5283b749a0b210c9c015a35f6da7b315da36698
Luis Carlin <luiscarlin@gmail.com>
2017-02-10 18:54:21 -0500

    prompt with vcs from zsh

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,3 +1,28 @@
-# Initialize advanced prompt support
-autoload -U promptinit
-promptinit
+autoload -Uz vcs_info
+
+zstyle ':vcs_info:*' stagedstr '%F{green} ●'
+zstyle ':vcs_info:*' unstagedstr '%F{yellow} ●'
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
+zstyle ':vcs_info:*' enable git svn
+
+theme_precmd () {
+    if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
+        zstyle ':vcs_info:*' formats ' (%b%c%u%B%F{green})'
+    } else {
+        zstyle ':vcs_info:*' formats ' (%b%c%u%B%F{red}●%F{green})'
+    }
+
+    vcs_info
+}
+
+setopt prompt_subst
+
+PROMPT='
+[%n@%m ${MAGENTA}%~${MAGENTA}${RESET}]
+%D{%H:%M}${CYAN}${vcs_info_msg_0_}${RESET} $ '
+
+autoload -U add-zsh-hook
+add-zsh-hook precmd  theme_precmd
+
+unset LSCOLORS
--- .zshrc
+++ .zshrc
@@ -1,11 +1,11 @@
+source ~/.zsh/functions.zsh
 source ~/.zsh/aliases.zsh
 source ~/.zsh/colors.zsh
 source ~/.zsh/completion.zsh
 source ~/.zsh/exports.zsh
-source ~/.zsh/functions.zsh
 source ~/.zsh/history.zsh
-source ~/.zsh/prompt.zsh
 source ~/.zsh/setopt.zsh
+source ~/.zsh/prompt.zsh
 
 # load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
