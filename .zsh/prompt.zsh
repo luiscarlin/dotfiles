ab6b102bab6c3a7b5203b2c892956eddd6fb6257
Luis Carlin <luiscarlin@gmail.com>
2017-02-11 10:21:22 -0500

    better prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,28 +1,11 @@
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' stagedstr '%F{green} ●'
-zstyle ':vcs_info:*' unstagedstr '%F{yellow} ●'
-zstyle ':vcs_info:*' check-for-changes true
-zstyle ':vcs_info:(sv[nk]|bzr):*' branchformat '%b%F{1}:%F{11}%r'
-zstyle ':vcs_info:*' enable git svn
-
-theme_precmd () {
-    if [[ -z $(git ls-files --other --exclude-standard 2> /dev/null) ]] {
-        zstyle ':vcs_info:*' formats ' (%b%c%u%B%F{green})'
-    } else {
-        zstyle ':vcs_info:*' formats ' (%b%c%u%B%F{red}●%F{green})'
-    }
-
-    vcs_info
+precmd() {
+  vcs_info
 }
 
 setopt prompt_subst
 
 PROMPT='
-[%n@%m ${MAGENTA}%~${MAGENTA}${RESET}]
+[${YELLOW}%n ${WHITE}at ${BLUE}%m ${WHITE}in ${MAGENTA}%~${RESET}]
 %D{%H:%M}${CYAN}${vcs_info_msg_0_}${RESET} $ '
-
-autoload -U add-zsh-hook
-add-zsh-hook precmd  theme_precmd
-
-unset LSCOLORS
