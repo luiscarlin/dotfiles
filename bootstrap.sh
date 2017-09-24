e0404495dbf5f5ebb5334082cceef3570af9be19
Luis Carlin <luiscarlin@gmail.com>
2017-09-23 21:43:04 -0400

    Added vimrc

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-current_dir=$(dirname $(readlink -f $0))
+if [[ $(uname) == "Linux" ]]; then
+  current_dir=$(dirname $(readlink -f $0))
+elif [[ $(uname) == "Darwin" ]]; then
+  brew install coreutils
+  current_dir=$(dirname $(greadlink -f $0))
+else
+  echo "What is this OS?"
+  exit 1
+fi
 
 ln -sfn ${current_dir}/.zsh ~/.zsh
 
 ln -sf ${current_dir}/.bashrc ~/.bashrc
 ln -sf ${current_dir}/.gitconfig ~/.gitconfig
 ln -sf ${current_dir}/.gitattributes ~/.gitattributes
+ln -sf ${current_dir}/.vimrc ~/.vimrc
 ln -sf ${current_dir}/.zshrc ~/.zshrc
 
 echo done
