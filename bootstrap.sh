b3203829c06581057947955752e1f138b3867bdb
Luis Carlin <luiscarlin@gmail.com>
2017-09-26 06:31:05 -0400

    Fix typo and better message

--- bootstrap.sh
+++ bootstrap.sh
@@ -22,11 +22,12 @@ ln -sf ${current_dir}/.vimrc ~/.vimrc
 ln -sf ${current_dir}/.zshrc ~/.zshrc
 
 if [[ ! -f ${current_dir}/.exports-path ]]; then
-  echo "${current_dir}/.exports-path not found"
+  echo "${current_dir}/.exports-path not found, so creating new one"
   touch ${current_dir}/.exports-path
-  echo "Created new .exports-path file in ${current_dir}"
-  echo "Follow the sample file ${current_dir}/.exports-path.sample"
-  echo "Add all your sysmem specific exports and path to ${current_dir}/.exports-path"
+
+  echo.
+  echo "Follow the sample file ${current_dir}/.exports-path.sample to use exports and path"
+  echo "Add all your system specific exports and path to ${current_dir}/.exports-path"
 fi
 
 ln -sf ${current_dir}/.exports-path ~/.exports-path
